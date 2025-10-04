da35bfeba167b7ff913ac5bc9081bc79aa344916
playoh <wazoski05@gmail.com>
2025-10-04 23:17:13 +0900

    style: button add

--- src/App.js
+++ src/App.js
@@ -7,16 +7,18 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello World!  
+          Hello World!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Yeon Ju's React Page
-        </a>
+        <button>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >Yeon Ju's React Page
+          </a>
+        </button>
+
       </header>
     </div>
   );
